4702cf8ea4e6c643b85c82113626180ca4101d3f
alex-smith <azphreal19@protonmail.com>
2018-01-20 23:18:02 +1100

    Expansion of libraries

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+#![recursion_limit="128"]
+
 #[macro_use]
 extern crate failure;
 extern crate futures;
 extern crate hyper;
 extern crate serde;
+#[macro_use]
 extern crate serde_json as json;
 extern crate tokio_core as tokio;
 extern crate hyper_tls;
@@ -12,8 +15,13 @@ extern crate log;
 extern crate md5;
 extern crate rand;
 
+mod macros;
+mod test_util;
 pub mod error;
 pub mod sunk;
+pub mod library;
+pub mod song;
+pub mod playlist;
 
 #[cfg(test)]
 mod tests {}
