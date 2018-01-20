ecc9eca0e80bd2a184b539e49863b0925cfca19e
alex-smith <azphreal19@protonmail.com>
2018-01-20 18:14:52 +1100

    Hyper error, rustfmt, neat little macro

--- src/error.rs
+++ src/error.rs
@@ -6,25 +6,23 @@ pub type Result<T> = ::std::result::Result<T, self::Error>;
 
 #[derive(Debug, Fail)]
 pub enum Error {
-    #[fail(display = "Unknown error: {}", _0)]
-    UnknownError(&'static str),
-    #[fail(display = "Invalid URL: {}", _0)]
-    InvalidUrl(#[cause] hyper::error::UriError),
-    #[fail(display = "IO error: {}", _0)]
-    Io(#[cause] io::Error),
-    #[fail(display = "API error: {}", _0)]
-    ServerError(String)
-    // InvalidUrl(&'static str),
+    #[fail(display = "Unknown error: {}", _0)] UnknownError(&'static str),
+    #[fail(display = "Invalid URL: {}", _0)] InvalidUrl(#[cause] hyper::error::UriError),
+    #[fail(display = "IO error: {}", _0)] Io(#[cause] io::Error),
+    #[fail(display = "API error: {}", _0)] ServerError(String), // InvalidUrl(&'static str)
+    #[fail(display = "Connection error: {}", _0)] HyperError(#[cause] hyper::Error),
 }
 
-impl From<hyper::error::UriError> for Error {
-    fn from(err: hyper::error::UriError) -> Error {
-        Error::InvalidUrl(err)
+macro_rules! box_err {
+    ($err:ty, $to:ident) => {
+        impl From<$err> for Error {
+            fn from(err: $err) -> Error {
+                Error::$to(err)
+            }
+        }
     }
 }
 
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::Io(err)
-    }
-}
+box_err!(hyper::Error, HyperError);
+box_err!(hyper::error::UriError, InvalidUrl);
+box_err!(io::Error, Io);
