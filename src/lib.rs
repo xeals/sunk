e4b42581611696875eefffbfc724509d78781f13
alex-smith <azphreal19@protonmail.com>
2018-01-20 19:05:42 +1100

    Crate additions

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ extern crate hyper;
 extern crate serde;
 extern crate serde_json as json;
 extern crate tokio_core as tokio;
+extern crate hyper_tls;
+#[macro_use]
+extern crate log;
 
 extern crate md5;
 extern crate rand;
